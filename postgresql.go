6d2426f4f485abd783aa5dda84442c3b2c2e18d2
Abhi Yerra <abhi@berkeley.edu>
2015-11-26 01:57:50 -0800

    added postgresql scaffold

---
+++ postgresql.go
@@ -0,0 +1,10 @@
+package main
+
+/*
+# Makes servers like postgresql and apache more scalable.
+
+kernel.sched_migration_cost_ns = 5000000
+kernel.sched_autogroup_enabled = 0
+
+/etc/sysctl.d/99-forking-servers.conf
+*/
