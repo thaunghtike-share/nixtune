f98676311dedc7c9ebca92f136c66a8afd77d280
Abhi Yerra <abhi@berkeley.edu>
2016-01-12 00:15:25 -0500

    Added website

---
+++ website/js/autotune_curl.ts
@@ -0,0 +1,25 @@
+import {Component} from 'angular2/core';
+import {NgForm}    from 'angular2/common';
+
+@Component({
+    // Declare the tag name in index.html to where the component attaches
+    selector: 'autotune-curl',
+    // Location of the template for this component
+    template: `
+<div>
+<label>Server Profile:</label>
+<select [(ngModel)]="selectedProfile" class="form-control">
+<option *ngFor="#p of profiles" [value]="p">{{p}}</option>
+</select>
+</div>
+<br>
+<code>\\curl -sSL https://anatma.co/autotune/install.sh | bash -s {{selectedProfile}}</code>`
+})
+export class AutotuneCurl {
+    profiles: []string = [
+        "golang",
+        "nodejs",
+    ];
+    // Default profile to use.
+    selectedProfile: string = 'golang';
+}
