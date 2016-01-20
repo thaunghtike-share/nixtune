a3683edc2e152de3b298ba830d5cf87b95f11fff
Abhi Yerra <abhi@berkeley.edu>
2016-01-20 02:13:20 -0500

    Changed the website

--- website/install.sh
+++ website/install.sh
@@ -4,14 +4,17 @@ VERSION=0.5.1
 
 function download_autotune {
     curl -o /tmp/autotune.tar.gz https://assets.anatma.co/autotune/${VERSION}/autotune-${VERSION}.tar.gz
-    cd /tmp && tar jxvf autotune.tar.gz
+    cd /tmp && tar zxvf autotune.tar.gz
     sudo mv autotune /usr/local/bin/autotune
 }
 
 function install_autotune {
     profile=$1 ; shift
 
-    autotune signature $profile
+    echo "Autotune WILL NOT update the values."
+    echo "First verify that the values being changed are okay."
+    echo "Run it again with: \"autotune signature $profile\""
+    autotune signature -write=false $profile
 }
 
 download_autotune
