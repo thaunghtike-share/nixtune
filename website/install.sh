5a6f33311dd25ae483b6c82ccbf3c3a760b7ebd2
Abhi Yerra <abhi@berkeley.edu>
2016-01-20 02:25:28 -0500

    Docs

--- website/install.sh
+++ website/install.sh
@@ -2,6 +2,16 @@
 
 VERSION=0.5.2
 
+function welcome_autotune {
+    echo "Welcome to Anatma Autotune ($VERSION) Install"
+    echo "Copyright (c) 2015-2016. Abhi Yerra."
+    echo "https://anatma.co/autotune"
+    echo ""
+    echo "We will be installing Autotune on your system into the following path:"
+    echo "  - /usr/local/bin"
+    echo ""
+}
+
 function download_autotune {
     curl -o /tmp/autotune.tar.gz https://assets.anatma.co/autotune/${VERSION}/autotune-${VERSION}.tar.gz
     cd /tmp && tar zxvf autotune.tar.gz
@@ -11,11 +21,16 @@ function download_autotune {
 function install_autotune {
     profile=$1 ; shift
 
-    echo "Autotune WILL NOT update the values."
-    echo "First verify that the values being changed are okay."
+    echo "Since we are installing from the web Anatma Autotune"
+    echo "WILL NOT update the system values. It will only display"
+    echo "what values will change."
+    echo ""
+    echo "Verify that the values being changed are okay."
     echo "Run it again with: \"autotune signature $profile\""
+    echo ""
     autotune signature -write=false $profile
 }
 
+welcome_autotune
 download_autotune
 install_autotune "$@"
