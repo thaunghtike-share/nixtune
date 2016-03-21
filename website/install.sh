b52dd64cd021685a04c92dea6b59f6b46780fa30
Abhi Yerra <abhi@berkeley.edu>
2016-03-21 06:12:40 +0000

    Simplified installation

--- website/install.sh
+++ website/install.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
-VERSION=0.5.4
+VERSION=0.1.0
 
 function welcome_strum {
     echo "Welcome to Acksin STRUM ($VERSION) Installation"
     echo "Copyright (c) 2016. Acksin, LLC."
     echo "https://acksin.co/strum"
     echo ""
-    echo "STRUM will be installed in the following location:"
-    echo ""
-    echo "    /usr/local/bin/strum"
-    echo ""
 }
 
 function download_strum {
+    echo "Downloading for $(uname)-$(uname -i)"
     curl -s -o /tmp/strum.tar.gz https://assets.acksin.com/strum/${VERSION}/strum-${VERSION}-$(uname)-$(uname -i).tar.gz
-    cd /tmp && tar zxf strum.tar.gz
-    sudo mv strum /usr/local/bin/strum
+    tar zxf strum.tar.gz
 }
 
 welcome_strum
 download_strum
-install_strum "$@"
