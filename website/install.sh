01f6deb789f6d770fea1f79561da48ef751f1b4c
Abhi Yerra <abhi@berkeley.edu>
2016-03-21 06:25:39 +0000

    Fixed issues with downloading STRUM

--- website/install.sh
+++ website/install.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
+PRODUCT=strum
 VERSION=0.1.0
 
 function welcome_strum {
     echo "Welcome to Acksin STRUM ($VERSION) Installation"
     echo "Copyright (c) 2016. Acksin, LLC."
-    echo "https://acksin.co/strum"
+    echo "https://acksin.com/strum"
     echo ""
 }
 
 function download_strum {
-    echo "Downloading for $(uname)-$(uname -i)"
-    curl -s -o /tmp/strum.tar.gz https://assets.acksin.com/$(PRODUCT)/${VERSION}/$(PRODUCT)-$(uname)-$(uname -i)-${VERSION}.tar.gz
+    echo "Downloading STRUM for $(uname)-$(uname -i)"
+    curl -s -o /tmp/strum.tar.gz https://assets.acksin.com/${PRODUCT}/${VERSION}/${PRODUCT}-$(uname)-$(uname -i)-${VERSION}.tar.gz
     tar zxf strum.tar.gz
 }
 
