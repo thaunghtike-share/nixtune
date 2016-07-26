164bfc7dbae812268e90319054e5a5af7d079bac
Abhi Yerra <abhi@berkeley.edu>
2016-07-26 22:12:46 +0000

    networking procfs changes

--- ai/mental_models/networking.py
+++ ai/mental_models/networking.py
@@ -56,3 +56,135 @@ class Networking(object):
         return {
             "/proc/sys/net/core/rmem_max": "16777216"
         }
+
+    def procfs_net_core_wmem_max(self):
+        """
+        The size of the buffer for all the sockets. 16MB per socket.
+        """
+        return {
+            "/proc/sys/net/core/wmem_max": "16777216",
+        }
+
+    def procfs_net_ipv4_tcp_rmem(self):
+        """
+        (min, default, max): The sizes of the receive buffer for the IP protocol.
+        """
+
+        return {
+            "/proc/sys/net/ipv4/tcp_rmem": "4096 87380 16777216",
+        }
+
+    def procfs_net_ipv4_tcp_wmem(self):
+        """
+        (min, default, max): The sizes of the write buffer for the IP protocol.
+        """
+
+        return {
+            "/proc/sys/net/ipv4/tcp_wmem": "4096 65536 16777216",
+        }
+
+    def procfs_net_ipv4_tcp_max_syn_backlog(self):
+        """
+        Increase the number syn requests allowed. Sets how many half-open connections to backlog queue
+        """
+
+        return {
+            "/proc/sys/net/ipv4/tcp_max_syn_backlog": "20480",
+        }
+
+    def procfs_net_ipv4_tcp_syncookies(self):
+        """
+        Security to prevent DDoS attacks. http://cr.yp.to/syncookies.html
+        """
+
+        return {
+            "/proc/sys/net/ipv4/tcp_syncookies": "1",
+        }
+
+    def procfs_net_ipv4_tcp_no_metrics_save(self):
+        """
+        TCP saves various connection metrics in the route cache when the
+        connection closes so that connections established in the near future
+        can use these to set initial conditions. Usually, this increases
+        overall performance, but may sometimes cause performance
+        degradation.
+        """
+
+        return {
+            "/proc/sys/net/ipv4/tcp_no_metrics_save": "1",
+        }
+
+    def procfs_net_core_somaxconn(self):
+        """
+        The maximum number of queued sockets on a connection.
+        """
+
+        return {
+            "/proc/sys/net/core/somaxconn": "16096",
+        }
+
+    def procfs_net_core_netdev_max_backlog(self):
+        """
+        The number of incoming connections on the backlog queue. The maximum
+        number of packets queued on the INPUT side.
+        """
+
+        return {
+            "/proc/sys/net/core/netdev_max_backlog": "30000",
+        }
+
+    def procfs_net_ipv4_tcp_max_tw_buckets(self):
+        """
+        Increase the tcp-time-wait buckets pool size to prevent simple DOS attacks
+        """
+
+        return {
+            "/proc/sys/net/ipv4/tcp_max_tw_buckets": "400000",
+        }
+
+    def procfs_net_ipv4_tcp_syn_retries(self):
+        """
+        Number of times initial SYNs for a TCP connection attempt will
+        be retransmitted for outgoing connections.
+        """
+
+        return {
+            "/proc/sys/net/ipv4/tcp_syn_retries": "2",
+        }
+
+    def procfs_net_ipv4_tcp_synack_retries(self):
+        """
+        This setting determines the number of SYN+ACK packets sent before
+        the kernel gives up on the connection
+        """
+
+        return {
+            "/proc/sys/net/ipv4/tcp_synack_retries": "2",
+        }
+
+    def procfs_net_netfilter_nf_conntrack_max(self):
+        """
+        The max is double the previous value.
+        https://wiki.khnet.info/index.php/Conntrack_tuning
+        """
+
+        return {
+            "/proc/sys/net/netfilter/nf_conntrack_max": self.vars['nfConntrackMax'],
+        }
+
+    def procfs_net_ipv4_tcp_tw_reuse(self):
+        """
+        """
+
+        return {
+                "/proc/sys/net/ipv4/tcp_tw_reuse": "1",
+        }
+
+    def sysfs_nf_conntrack_hashsize(self):
+        """
+        """
+
+        return {
+        "/sys/module/nf_conntrack/parameters/hashsize": self.vars["nfConntrackMax"] / 4
+
+}
