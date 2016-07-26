43ad324e85f79ca06b487f41068f0484c6d72e16
Abhi Yerra <abhi@berkeley.edu>
2016-07-26 20:18:28 +0000

    Added header

--- ai/mental_models/memory.py
+++ ai/mental_models/memory.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2016 Acksin <hey@acksin.com>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 class Memory(object):
     def __init__(self, strum):
         self.strum = strum
