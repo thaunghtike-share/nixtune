fd0b89e496e905d3dee6f9d58cb271c26104a1d9
Abhi Yerra <abhi@berkeley.edu>
2016-08-08 23:42:50 +0000

    Show the Dashboard with machines

--- console/js/strum-dashboard.jsx
+++ console/js/strum-dashboard.jsx
@@ -13,13 +13,38 @@ var AcksinStrumDashboard  = React.createClass({
     }
   },
   componentDidMount: function() {
-    $.get(BridgeAPI + "/v1/strum/stats", function(result) {
+    $.get(BridgeAPI + "/v1/strum/nodes", function(result) {
+      var c = [];
+
+      for(var i = 0; i < result.length; i++) {
+        c.push(
+          <tr key={result[i].ID}>
+            <td><a href={"/console/strum/#/" + result[i].ID}>{result[i].InstanceID}</a></td>
+            <td>{result[i].InstanceType}</td>
+            <td>{result[i].CreatedAt}</td>
+          </tr>
+        );
+      }
+
+      this.setState({
+        content: c
+      });
     }.bind(this));
   },
   render: function() {
     return (
       <div>
         <table className="table">
+          <thead>
+            <tr>
+              <th>InstanceID</th>
+              <th>InstanceType</th>
+              <th>Last Updated</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.content}
+          </tbody>
         </table>
       </div>
     );
