88161081dc14a5683a0a129585e68f33dea04ff1
Abhi Yerra <abhi@berkeley.edu>
2016-08-10 00:12:27 +0000

    Create Autotune documentation

--- console/js/strum-autotune.jsx
+++ console/js/strum-autotune.jsx
@@ -8,10 +8,36 @@
 
 
 var AcksinStrumAutotune = React.createClass({
+  getInitialState: function() {
+    return {
+    };
+  },
+  componentDidMount: function() {
+    if(this.props.statsId == "") {
+      return;
+    }
+
+    $.get(BridgeAPI + "/v1/strum/autotune/" + this.props.statsId , function(result) {
+      $.get(result.URL, function(stats) {
+        this.setState(result);
+      }.bind(this));
+    }.bind(this));
+  },
   render: function() {
     return (
       <div>
         <AcksinStrumNav statsId={this.props.statsId} />
+
+        <p>
+          After analyzing the node the following changes can give a boost in performance.
+          We are keeping an eye out and additional tuning will appear here.
+        </p>
+
+        <h2>ProcFS</h2>
+        <AcksinStrumTable property={this.state.ProcFS} />
+
+        <h2>SysFS</h2>
+        <AcksinStrumTable property={this.state.SysFS} />
       </div>
     );
   }
