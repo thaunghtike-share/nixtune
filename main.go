7c3484674e3dd13927760d09bf9141da27e0c119
Abhi Yerra <abhi@berkeley.edu>
2017-06-22 23:31:14 -0700

    Moved things around

--- main.go
+++ main.go
@@ -1,49 +1,7 @@
-/*
- * Copyright (C) 2016 opszero,LLC <abhi@opszero.com>
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/mitchellh/cli"
-)
-
-var (
-	version = "0.0"
-)
-
-func copyright() string {
-	return fmt.Sprintf(`opszero %s.
-Copyright (c) 2016. opszero, LLC.
-https://www.opszero.com
-`, version)
-}
+import "github.com/opszero/autotune/cmd"
 
 func main() {
-	c := cli.NewCLI("opszero", version)
-	c.Args = os.Args[1:]
-
-	c.Commands = map[string]cli.CommandFactory{
-		"agent": func() (cli.Command, error) {
-			return &agent{}, nil
-		},
-		"output": func() (cli.Command, error) {
-			return &output{}, nil
-		},
-	}
-
-	exitStatus, err := c.Run()
-	if err != nil {
-		log.Println(err)
-	}
-
-	os.Exit(exitStatus)
+	cmd.Execute()
 }
