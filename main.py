22011509ba2a4d3284212edce5f03788cd063c35
Abhi Yerra <abhi@berkeley.edu>
2018-03-29 20:27:40 +0000

    Moved networking

--- main.py
+++ main.py
@@ -4,8 +4,44 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from decorators import *
-import mental_model
+import string
+
+def ai_feature(func):
+    def wrapper(self):
+        return func(self)
+
+    return wrapper
+
+def procfs_feature(func):
+    """
+    procfs_feature returns a dictionary if the current system doesn't match
+    the value we want it to have.
+    """
+
+    def wrapper(self, *args, **kwargs):
+        output = func(self, *args, **kwargs)
+
+        returned = {}
+        for k, v in output.items():
+            kernel = self.machine.stats['System']['Kernel']
+            if kernel.has_key(k) and not kernel[k] == v:
+                change = {
+                    'Current': kernel[k],
+                    'Replacement': v,
+                    'Docs': string.strip(func.__doc__),
+                }
+                returned = dict(returned.items() + [(k, change)])
+
+        return returned
+
+    return wrapper
+
+def sysfs_feature(func):
+    def wrapper(self):
+        return func(self)
+
+    return wrapper
+
 
 class Networking(mental_model.MentalModel):
     """
@@ -219,3 +255,10 @@ class Networking(mental_model.MentalModel):
         return {
             "/sys/module/nf_conntrack/parameters/hashsize": self.vars["nfConntrackMax"] / 4
         }
+
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
